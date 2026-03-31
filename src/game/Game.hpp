3614018ef33375c7b634e82d9b98448cd5f7f495
Joseph Roy <josephroy8778@gmail.com>
2026-04-01 02:46:23 +0530

    Completed score updation, winning and losing

--- src/game/Game.hpp
+++ src/game/Game.hpp
@@ -1,35 +1,36 @@
 #pragma once
-#include <entt/entt.hpp>
-#include <SFML/Graphics.hpp>
-#include "../map/Map.hpp"
 #include "../core/Components.hpp"
 #include "../core/Direction.hpp"
+#include "../map/Map.hpp"
+#include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
 
 enum class GameStatus { Playing, Dead, Respawn, GameOver, Win };
 
 class Game {
 public:
-    Game();
+  Game();
 
-    void handleInput(sf::Keyboard::Key key, bool pressed);
-    void update(float dt);
+  void handleInput(sf::Keyboard::Key key, bool pressed);
+  void update(float dt);
 
-    entt::registry& getRegistry() { return registry; }
-    const Map& getMap() const { return map; }
-    int getScore() const { return score; }
-    int getLives() const { return lives; }
-    GameStatus getStatus() const { return status; }
+  entt::registry &getRegistry() { return registry; }
+  const Map &getMap() const { return map; }
+  int getScore() const { return score; }
+  int getLives() const { return lives; }
+  GameStatus getStatus() const { return status; }
+  void addScore(int points) { score += points; }
 
 private:
-    entt::registry registry;
-    Map map;
-    int score = 0;
-    int lives = 3;
-    GameStatus status = GameStatus::Playing;
-    float respawnTimer = 0.f;
+  entt::registry registry;
+  Map map;
+  int score = 0;
+  int lives = 3;
+  GameStatus status = GameStatus::Playing;
+  float respawnTimer = 0.f;
 
-    void initEntities();
-    void spawnPacman();
-    void spawnGhosts();
-    void spawnDots();
+  void initEntities();
+  void spawnPacman();
+  void spawnGhosts();
+  void spawnDots();
 };
